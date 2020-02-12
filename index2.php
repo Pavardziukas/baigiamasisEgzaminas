8d05cd8fefcd54ad4d4e6213e1d314efb4be5dae
Pavardziukas <55126024+Pavardziukas@users.noreply.github.com>
2020-02-12 23:05:29 +0200

    Add files via upload
    maps added

--- index2.php
+++ index2.php
@@ -54,7 +54,7 @@ if (!App::$session->userLoggedIn()) {
         </div>
     </div>
 
-
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.2194261566747!2d25.33569661534373!3d54.723355078378496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010221!5e0!3m2!1sen!2slt!4v1581540742301!5m2!1sen!2slt" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </main>
 
 <!-- Footer -->
